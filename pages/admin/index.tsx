afd8f3b9a1ee5a749c9c63902e416977966e82d7
wisnu-aji <ajiw8022@gmail.com>
2022-05-21 13:14:36 +0700

    fix deploy error

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,18 +1,21 @@
 import { useSession, signOut, signIn } from "next-auth/react";
-export default function component() {
+import { FC } from "react";
+
+const Admin: FC = () => {
   const { data: session } = useSession();
   if (session) {
     return (
-      <>
+      <div>
         Signed in as {JSON.stringify(session)} <br />
         <button onClick={() => signOut()}>sign out</button>
-      </>
+      </div>
     );
   }
   return (
-    <>
+    <div>
       not signed in <br />
       <button onClick={() => signIn()}>sign in</button>
-    </>
+    </div>
   );
-}
+};
+export default Admin;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Iklan } from "../components/Iklan";
 import Ombak from "../components/Ombak";
 import OmbakFooter from "../components/OmbakFooter";
