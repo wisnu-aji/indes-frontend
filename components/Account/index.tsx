95e3bc10e70c339674791e0ef7249df2c0babd5f
wisnu-aji <ajiw8022@gmail.com>
2022-05-27 15:00:44 +0700

    remove specified google provider

--- components/Account/index.tsx
+++ components/Account/index.tsx
@@ -1,33 +1,32 @@
-import { useSession, signIn, signOut } from "next-auth/react"
-import React, { FC, useState } from "react"
-import style from "./style.module.css"
+import { useSession, signIn, signOut } from "next-auth/react";
+import React, { FC, useState } from "react";
+import style from "./style.module.css";
 
 const Logout: FC = () => {
-  const data = useSession()
+  const data = useSession();
 
   return (
     <div>
       <button onClick={() => signOut()}>Keluar</button>
     </div>
-  )
-}
+  );
+};
 
 export const Account: FC = () => {
-  const [show, setShow] = useState(false)
-  const { data } = useSession()
+  const [show, setShow] = useState(false);
+  const { data } = useSession();
   if (!data || !data.user)
     return (
-      <div className={style.container} onClick={() => signIn('google')}>
-        <button className={style.login} onClick={() => signIn('google')}>Masuk</button>
+      <div className={style.container} onClick={() => signIn()}>
+        <button className={style.login} onClick={() => signIn()}>
+          Masuk
+        </button>
       </div>
-    )
+    );
   return (
     <div className={style.container} onClick={() => setShow((v) => !v)}>
       <img className={style.userPicture} src={data.user.image!} />
-      <div className={style.userName}>
-
-      {show ? <Logout /> : data.user.name}
-      </div>
+      <div className={style.userName}>{show ? <Logout /> : data.user.name}</div>
     </div>
-  )
-}
+  );
+};
