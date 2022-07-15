0fccdd4610ea320a1713bf53cc34463610c8ad69
wisnu-aji <ajiw8022@gmail.com>
2022-07-15 17:37:41 +0700

    edit use pelanggan baru

--- hooks/use-pelanggan-baru.ts
+++ hooks/use-pelanggan-baru.ts
@@ -6,6 +6,7 @@ export interface PelangganBaruContext {
     telepon: string;
     paket: string;
     pemasangan: string;
+    password: string;
   };
   setPelanggan: (pelanggan: {
     nama: string;
@@ -13,6 +14,7 @@ export interface PelangganBaruContext {
     telepon: string;
     paket: string;
     pemasangan: string;
+    password: string;
   }) => void;
 }
 export const PelangganContext = createContext<PelangganBaruContext>({
@@ -22,6 +24,7 @@ export const PelangganContext = createContext<PelangganBaruContext>({
     telepon: "",
     paket: "",
     pemasangan: "",
+    password:"",
   },
   setPelanggan: () => {},
 });
